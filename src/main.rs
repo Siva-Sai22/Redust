3e56daffce9ba20f15c7a881a1fa5dba63b60e73
sivasai <kollakotasivasai@gmail.com>
2025-07-17 12:47:54 +0000

    Add response for multiple ping

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,25 @@
 #![allow(unused_imports)]
-use std::{io::Write, net::TcpListener};
+use std::{
+    io::{Read, Write},
+    net::TcpListener,
+};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    
+
     for stream in listener.incoming() {
         match stream {
             Ok(mut _stream) => {
-                _stream.write_all(b"+PONG\r\n").unwrap();
                 println!("accepted new connection");
+                let mut buf = String::new();
+                let bytes_read = _stream.read_to_string(&mut buf).unwrap();
+                let cnt = buf.matches("PING").count();
+                for _ in 0..cnt {
+                    _stream.write_all(b"+PONG\r\n").unwrap();
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
