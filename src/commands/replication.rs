5d5d1d8400b5855471da32f9fffae8971b57c994
sivasai <kollakotasivasai@gmail.com>
2025-08-26 15:25:54 +0000

    codecrafters submit [skip ci]

--- src/commands/replication.rs
+++ src/commands/replication.rs
@@ -99,61 +99,8 @@ pub async fn handle_wait<W: AsyncWriteExt + Unpin>(
         stream.write_all(err_msg.as_bytes()).await?;
         return Ok(());
     }
-
-    // Parse arguments
-    let required_replicas = args[0].parse::<usize>().unwrap_or(0);
-    let timeout_ms = args[1].parse::<u64>().unwrap_or(0);
-
-    // Get the current master offset - this is what we want replicas to acknowledge
-    let current_master_offset = *state.master_replication_offset.lock().await;
-
-    // Create timeout deadline
-    let deadline = tokio::time::Instant::now() + tokio::time::Duration::from_millis(timeout_ms);
-
-    // Loop until we have enough replicas or timeout
-    let mut acknowledged_replicas = 0;
-
-    loop {
-        // Check how many replicas have sufficient offset
-        {
-            let replicas = state.replicas.lock().await;
-            acknowledged_replicas = replicas
-                .iter()
-                .filter(|replica| replica.offset >= current_master_offset)
-                .count();
-
-            // Exit if we have enough replicas
-            if acknowledged_replicas >= required_replicas {
-                break;
-            }
-        }
-
-        // Check if timeout occurred
-        if tokio::time::Instant::now() >= deadline {
-            break;
-        }
-
-        // Send GETACK to all replicas to try to get updated offsets
-        let getack_cmd = "*3\r\n$8\r\nREPLCONF\r\n$6\r\nGETACK\r\n$1\r\n*\r\n";
-        {
-            let mut replicas = state.replicas.lock().await;
-            if !replicas.is_empty() {
-                for replica in replicas.iter_mut() {
-                    let _ = replica.stream.write_all(getack_cmd.as_bytes()).await;
-                }
-
-                // Update master offset for the GETACK command
-                let cmd_len = getack_cmd.len() as u64;
-                let mut master_offset = state.master_replication_offset.lock().await;
-                *master_offset += cmd_len;
-            }
-        }
-
-        // Sleep a bit before checking again (50ms seems reasonable)
-        tokio::time::sleep(tokio::time::Duration::from_millis(50)).await;
-    }
-
-    // Return the count of replicas that acknowledged the commands
-    let response = format!(":{}\r\n", acknowledged_replicas);
+    
+    let replicas = state.replicas.lock().await;
+    let response = format!(":{}\r\n", replicas.len());
     stream.write_all(response.as_bytes()).await
 }
